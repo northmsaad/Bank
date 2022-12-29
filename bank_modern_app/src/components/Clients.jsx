0ee272ed3caad403f07a17639a519cbf5ead4f89
northmsaad <msddr010@gmail.com>
2022-12-29 13:57:59 +0500

    commit

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { clients } from "../constant";
+import styles from "../style";
 const Clients = () => {
   return (
     <div>
-      <h1>Client</h1>
+      <section className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+          {clients.map((client) => (
+            <div
+              key={client.id}
+              className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}>
+              <img
+                src={client.logo}
+                alt="Client"
+                className="sm:w-[192px] w-[100px] object-contain"
+              />
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
